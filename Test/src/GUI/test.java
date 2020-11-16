1344a2142e7198ad119359572cddcdab84c2f66f
Asaad Dadoush <asaaddaadouch246@gmail.com>
2020-11-16 09:13:27 +0300

    just test

---
+++ Test/src/GUI/test.java
@@ -0,0 +1,16 @@
+package GUI;
+
+	
+public class test{
+	
+	public static void main(String [] args) {
+		
+		int a = 10 ;
+		int b = 8;
+		
+		int max = (a > b) ? a : b ;
+		
+		System.out.println(max);
+	}
+	
+}
